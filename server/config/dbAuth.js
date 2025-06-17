139e2d27ef9a73b31b5d400a3c30603bd293c85b
Nandini Manoj Pathak <166193813+nandinipathak2005@users.noreply.github.com>
2025-06-17 14:19:42 +0530

    made some changes

--- server/config/dbAuth.js
+++ server/config/dbAuth.js
@@ -10,16 +10,36 @@
 // });
 
 // module.exports = authDB;
+// const { Sequelize } = require('sequelize');
+
+// const authDB = new Sequelize('bhrzxgaufey6jf8j6f0x', 'udcohdhmrcirdmbf', 'GfGPS2amdPVVCnCeojJn', {
+//   host: 'bhrzxgaufey6jf8j6f0x-mysql.services.clever-cloud.com',        // e.g., 'db4.freehosting.com'
+//   port: 3306,       // e.g., 3306 (without quotes if number)
+//   dialect: 'mysql',
+//   logging: false,
+//   define: {
+//     timestamps: false
+//   }
+// });
+
+// module.exports = authDB;
+require('dotenv').config();
+
 const { Sequelize } = require('sequelize');
 
-const authDB = new Sequelize('bhrzxgaufey6jf8j6f0x', 'udcohdhmrcirdmbf', 'GfGPS2amdPVVCnCeojJn', {
-  host: 'bhrzxgaufey6jf8j6f0x-mysql.services.clever-cloud.com',        // e.g., 'db4.freehosting.com'
-  port: 3306,       // e.g., 3306 (without quotes if number)
-  dialect: 'mysql',
-  logging: false,
-  define: {
-    timestamps: false
+const authDB = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    logging: false,
+    define: {
+      timestamps: false
+    }
   }
-});
+);
 
 module.exports = authDB;
